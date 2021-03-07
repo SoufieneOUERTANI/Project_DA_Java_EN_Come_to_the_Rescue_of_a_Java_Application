edb7b876348ebd2b06b5a40a97722f0d8a73a919
Soufiene OUERTANI <soufiene.ouertani@gmail.com>
2021-03-07 11:58:24 +0100

    Décompsoer la première classe de lecture du fichier

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,55 +1,19 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.FileWriter;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0; // initialize to 0
-	private static int rashCount = 0; // initialize to 0
-	private static int pupilCount = 0; // initialize to 0
-
 	public static void main(String args[]) throws Exception {
-		Map<String, Integer> symptomsList = new HashMap<String, Integer>();
-
-		try (// first get input
-				BufferedReader reader = new BufferedReader(new FileReader("Project02Eclipse/symptoms.txt"))) {
-			String line = reader.readLine();
-
-			// SOUE 77 int i = 0; // set i to 0
-			int headCount = 0; // counts headaches
-			while (line != null) {
-				// SOUE77 i++; // increment i
-				System.out.println("symptom from file: " + line);
-				if (symptomsList.get(line) != null)
-					symptomsList.put(line, symptomsList.get(line) + 1);
-				else
-					symptomsList.put(line, 1);
-				if (line.equals("headache")) {
-					headCount++;
-					System.out.println("number of headaches: " + headCount);
-				} else if (line.equals("rush")) {
-					rashCount++;
-				} else if (line.contains("pupils")) {
-					pupilCount++;
+		ReadSymptomDataFromFile readSymptomDataFromFile = new ReadSymptomDataFromFile("Project02Eclipse/symptoms.txt");
+		Map<String, Integer> symptomsList = readSymptomDataFromFile.GetSymptoms();
+		try (FileWriter writer = new FileWriter("Project02Eclipse/result.out")) {
+			if (!symptomsList.isEmpty()) {
+				for (Entry<String, Integer> entry : symptomsList.entrySet()) {
+					writer.write(entry.getKey() + ": " + entry.getValue() + "\n");
 				}
-
-				line = reader.readLine(); // get another symptom
 			}
-		}
-		// next generate output
-		FileWriter writer = new FileWriter("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
-		for (Entry<String, Integer> entry : symptomsList.entrySet()) {
-			String k = entry.getKey();
-			Integer v = entry.getValue();
-			System.out.println("Symptom: " + k + ", Value: " + v);
 		}
 	}
 }
--- Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -2,9 +2,8 @@ package com.hemebiotech.analytics;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Simple brute force implementation
@@ -13,35 +12,30 @@ import java.util.List;
 public class ReadSymptomDataFromFile implements ISymptomReader {
 
 	private String filepath;
-	
+
 	/**
 	 * 
-	 * @param filepath a full or partial path to file with symptom strings in it, one per line
+	 * @param filepath a full or partial path to file with symptom strings in it,
+	 *                 one per line
 	 */
-	public ReadSymptomDataFromFile (String filepath) {
+	public ReadSymptomDataFromFile(String filepath) {
 		this.filepath = filepath;
 	}
-	
+
 	@Override
-	public List<String> GetSymptoms() {
-		ArrayList<String> result = new ArrayList<String>();
-		
-		if (filepath != null) {
-			try {
-				BufferedReader reader = new BufferedReader (new FileReader(filepath));
-				String line = reader.readLine();
-				
-				while (line != null) {
-					result.add(line);
-					line = reader.readLine();
-				}
-				reader.close();
-			} catch (IOException e) {
-				e.printStackTrace();
+	public Map<String, Integer> GetSymptoms() throws Exception {
+		Map<String, Integer> result = new HashMap<String, Integer>();
+		try (BufferedReader reader = new BufferedReader(new FileReader(filepath))) {
+			String line = reader.readLine();
+			while (line != null) {
+				System.out.println("symptom from file: " + line);
+				if (result.get(line) != null)
+					result.put(line, result.get(line) + 1);
+				else
+					result.put(line, 1);
+				line = reader.readLine(); // get another symptom
 			}
 		}
-		
 		return result;
 	}
-
 }
