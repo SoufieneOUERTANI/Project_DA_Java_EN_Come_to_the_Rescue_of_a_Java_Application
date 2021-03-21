4ed7674ea926023dfa5089e2fa3a62cf908eb297
Soufiene OUERTANI <soufiene.ouertani@gmail.com>
2021-03-21 19:05:55 +0100

    Suppression des fichiers de doc

--- doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.hemebiotech.analytics"},{"l":"com.hemebiotech.analytics.business"},{"l":"com.hemebiotech.analytics.service"}];updateSearchResults();
